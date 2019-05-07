f07f719424fcd40663ca7caa63b0ca915a629d43
AeroX2 <james@ridey.email>
2019-05-07 18:00:50 +1000

    Noisy test

--- mnist_keras_test.py
+++ mnist_keras_test.py
@@ -22,30 +22,17 @@ def load_noisy_data(path='mnist-with-awgn.mat'):
 
     import scipy.io
     f = scipy.io.loadmat(path)
-    x_train, y_train = f['x_train'], f['y_train']
-    x_test, y_test = f['x_test'], f['y_test']
+    x_train, y_train = f['train_x'], f['train_y']
+    x_test, y_test = f['test_x'], f['test_y']
+
+    y_train = y_train.nonzero()[1]
+    y_test = y_test.nonzero()[1]
+
     return (x_train, y_train), (x_test, y_test)
 
 batch_size = 128
 num_classes = 10
 epochs = 20
-
-# the data, split between train and test sets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-# (x_train, y_train), (x_test, y_test) = load_noisy_data()
-
-x_train = x_train.reshape(60000, 784)
-x_test = x_test.reshape(10000, 784)
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
-
-# convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
 
 from keras import backend as K
 from keras.layers import Layer
@@ -81,25 +68,32 @@ class StochasticLayer(Layer):
     def compute_output_shape(self, input_shape):
         return input_shape
 
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = x_train.reshape(60000, 784)
+x_test = x_test.reshape(10000, 784)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+print('Normal model')
 model = Sequential()
-model.add(StochasticLayer(784, input_shape=(784,), trainable=False))
 model.add(Dense(512, input_shape=(784,), activation='relu'))
 model.add(Dropout(0.2))
 model.add(Dense(512, activation='relu'))
 model.add(Dropout(0.2))
 model.add(Dense(num_classes, activation='softmax'))
 
-model.summary()
-
 model.compile(loss='categorical_crossentropy',
               optimizer=RMSprop(),
               metrics=['accuracy'])
-
-# from tensorflow.python import debug as tf_debug
-# sess = K.get_session()
-# sess = tf_debug.LocalCLIDebugWrapperSession(sess)
-# K.set_session(sess)
-
 history = model.fit(x_train, y_train,
                     batch_size=batch_size,
                     epochs=epochs,
@@ -108,3 +102,32 @@ history = model.fit(x_train, y_train,
 score = model.evaluate(x_test, y_test, verbose=1)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
+
+print('Stochastic model')
+model_stochastic = Sequential()
+model_stochastic.add(StochasticLayer(784, input_shape=(784,), trainable=False))
+model_stochastic.add(Dense(512, input_shape=(784,), activation='relu'))
+model_stochastic.add(Dropout(0.2))
+model_stochastic.add(Dense(512, activation='relu'))
+model_stochastic.add(Dropout(0.2))
+model_stochastic.add(Dense(num_classes, activation='softmax'))
+
+model_stochastic.compile(loss='categorical_crossentropy',
+              optimizer=RMSprop(),
+              metrics=['accuracy'])
+history = model_stochastic.fit(x_train, y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_data=(x_test, y_test))
+score = model_stochastic.evaluate(x_test, y_test, verbose=1)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+(x_train, y_train), (x_test, y_test) = load_noisy_data()
+score = model.evaluate(x_test, y_test, verbose=1)
+print('Normal Noisy Test loss:', score[0])
+print('Normal Noisy Test accuracy:', score[1])
+score = model_stochastic.evaluate(x_test, y_test, verbose=1)
+print('Stochastic Noisy Test loss:', score[0])
+print('Stochastic Noisy Test accuracy:', score[1])
