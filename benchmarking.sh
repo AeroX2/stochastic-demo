7b0668b998ad288db9831f15def61e7565ace86f
Your Name <you@example.com>
2019-05-27 10:45:27 +0000

    Remove unnessessary pyc and __pycache__ files

--- benchmarking.sh
+++ benchmarking.sh
@@ -8,5 +8,5 @@ for i in {1..20}
 do
 	python3 benchmark.py mnist_keras_size_test.py 10
 	python3 benchmark.py mnist_keras_epoch_test.py 20
-	python3 benchmark.py mnist_keras_dataset_test.py 20
+	python3 benchmark.py mnist_keras_dataset_test.py 10
 done
